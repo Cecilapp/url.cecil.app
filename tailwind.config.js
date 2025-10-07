9ee445d8c28b10971a5c817f6bc526582c60eef0
Arnaud Ligny <arnaud@ligny.fr>
2025-10-07 04:07:46 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    require('./themes/applanding/tailwind.preset.js')
-  ],
-  content: [
-    './layouts/**/*.html.twig',
-    './themes/**/layouts/**/*.html.twig',
-  ]
-}
